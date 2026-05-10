a5b5724560f4f00ba0052b8ff1cc9f6270a5140c
farulivan <farul@nabitu.id>
2026-05-10 17:06:51 +0700

    feat: add help and exit commands with command registry system

--- repl.go
+++ repl.go
@@ -19,10 +19,41 @@ func startRepl() {
 		}
 
 		commandName := words[0]
-		fmt.Printf("Your command was: %s\n", commandName)
+
+		command, exist := getCommands()[commandName]
+		if !exist {
+			fmt.Println("unknown command")
+			continue
+		}
+
+		err := command.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
 }
 
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
